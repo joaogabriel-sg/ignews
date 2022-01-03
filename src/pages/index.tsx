4539ad52d25c29d0f796a7f260df691599d2a2ac
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-03 20:09:07 -0300

    feat: add app global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
   );
 }
