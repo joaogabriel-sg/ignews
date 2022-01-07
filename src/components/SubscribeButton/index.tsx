502610b1f458f711e4dfc38d1b9369ccb5363d08
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-07 20:15:32 -0300

    feat: create stripe subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from "next-auth/react";
+
+import { api } from "@services/api";
+import { getStripeJs } from "@services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -5,8 +10,31 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
